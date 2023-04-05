719e5f7cc1265da7422e9fb72eafead5fb469d1d
Yann Kamche <yannkamche@gmail.com>
2023-04-05 21:05:06 +0100

    Added the calculating and toggling functionalities

--- Calculator/Swipeable Light Dark Theme Calculator/js/script.js
+++ Calculator/Swipeable Light Dark Theme Calculator/js/script.js
@@ -1,6 +1,7 @@
 const display = document.querySelector('#display');
 const buttons = document.querySelectorAll('button');
 
+
 buttons.forEach((item) => {
     item.onclick = () => {
         if (item.id == 'clear') {
